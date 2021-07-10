b3bd1e7f23bee31092002fbfccfe087ecf830f44
kaushiki29 <kaushikiraj029@gmail.com>
2021-07-10 07:09:53 +0000

    Unit tests added

--- backend/msteams/authentication/tests.py
+++ backend/msteams/authentication/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+
+class AuthTests(APITestCase):
+    def setUp(self):
+        # create user
+        self.user = User.objects.create_user(username="joa123@gmail.com", password="password")
+        self.user.first_name = "Joanne"
+        self.user.last_name = "Williams"
+        self.user.save() 
+        token = Token.objects.get_or_create(user = self.user)[0]
+
+    def test_invalid_login_credentials(self):
+        url = reverse('user_login')
+        data = {'email': "joa123@gmail.com", 'password': 'passww'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_valid_login_credentials(self):
+        url = reverse('user_login')
+        data = {'email': "joa123@gmail.com", 'password': 'pass'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse('logout')
+        data = {}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout_without_login(self):
+        url = reverse('logout')
+        data = {}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
